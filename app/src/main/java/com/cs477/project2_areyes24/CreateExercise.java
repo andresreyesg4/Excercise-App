4d7379a947291532ee678b998be51d7b334b3681
andresreyesg4 <Andres.reyes121014@gmail.com>
2021-11-02 02:07:09 -0400

    added all activities and database

---
+++ app/src/main/java/com/cs477/project2_areyes24/CreateExercise.java
@@ -0,0 +1,14 @@
+package com.cs477.project2_areyes24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_exercise);
+    }
+}
--- app/src/main/java/com/cs477/project2_areyes24/MainActivity.java
+++ app/src/main/java/com/cs477/project2_areyes24/MainActivity.java
@@ -2,6 +2,7 @@ package com.cs477.project2_areyes24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button workout;
     Button edit;
+    DatabaseOpenHelper workout_db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,16 +19,28 @@ public class MainActivity extends AppCompatActivity {
         // initiate the buttons on the screen
         workout = findViewById(R.id.workout);
         edit = findViewById(R.id.edit_mode);
-    }
 
+        // initiate the database
+        workout_db = new DatabaseOpenHelper(this);
+    }
 
     // function to be called when the button workout is clicked.
     public void onWorkout(View view){
+        // initialize a new activity
+        Intent intent = new Intent(this, Workout.class);
+
+        //Start the activity
+        startActivity(intent);
 
     }
 
     // function to be called when the button edit is clicked
     public void onEdit(View view){
+        // initialize a new activity
+        Intent intent = new Intent(this, EditWorkout.class);
+
+        //Start the activity
+        startActivity(intent);
 
     }
 
