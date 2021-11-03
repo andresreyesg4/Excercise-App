dd229f7da97ed1288fb123f5f1ce6b33562f14ee
andresreyesg4 <Andres.reyes121014@gmail.com>
2021-11-03 00:46:11 -0400

    Completed update exercise and started edit workout view of the list

--- app/src/main/java/com/cs477/project2_areyes24/EditWorkout.java
+++ app/src/main/java/com/cs477/project2_areyes24/EditWorkout.java
@@ -2,21 +2,34 @@ package com.cs477.project2_areyes24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class EditWorkout extends AppCompatActivity {
     Button add;
     ListView listView;
+    DatabaseOpenHelper helper;
+    static final int UPDATED = 200;
+    static final int ADDED = 201;
+    private SimpleCursorAdapter adapter;
+    SQLiteDatabase database;
+    Cursor cursor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_workout);
         listView = findViewById(R.id.current_exercises);
+        helper = new DatabaseOpenHelper(this);
+        cursor = helper.readItems();
 
         // set the button click listener for new exercises
         add = findViewById(R.id.add);
@@ -26,11 +39,63 @@ public class EditWorkout extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                startActivity(intent);
+                cursor.moveToPosition(i);
+                int id = cursor.getInt(0);
+                intent.putExtra("_id", id);
+                intent.putExtra("index", i);
+                startActivityForResult(intent, UPDATED);
             }
         });
 
 
+    }
+
+    private final class LoadDB extends AsyncTask<String, Void, Cursor> {
+        @Override protected void onPostExecute(Cursor data){
+            adapter = new SimpleCursorAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,
+                    data,
+                    new String[]{"name", "sets", "reps", "weight", "notes"},
+                    new int[] {android.R.id.text1}, 0);
+            cursor = data;
+            listView.setAdapter(adapter);
+        }
+        @Override
+        protected Cursor doInBackground(String... strings) {
+            database = helper.getWritableDatabase();
+            String[] columns = new String[]{"_id", helper.COL_NAME, helper.COL_REPS, helper.COL_SETS, helper.COL_WEIGHT, helper.COL_NOTES};
+            return database.query(helper.TABLE_NAME, columns, null, null, null, null, null);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == UPDATED){
+            Bundle extras = data.getExtras();
+            if(extras != null){
+                int reps = extras.getInt("reps");
+                int sets = extras.getInt("sets");
+                int weight = extras.getInt("weight");
+                String notes = extras.getString("notes");
+                String name = extras.getString("name");
+                int id = extras.getInt("_id");
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(helper.COL_REPS, reps);
+                contentValues.put(helper.COL_SETS, sets);
+                contentValues.put(helper.COL_WEIGHT, weight);
+                contentValues.put(helper.COL_NOTES, notes);
+                database.update(helper.TABLE_NAME, contentValues, "_id=? AND " + helper.COL_NAME + "=?", new String[]{Integer.toString(id), name});
+                cursor = helper.readItems();
+                adapter.swapCursor(cursor);
+            }
+        }else if(requestCode == ADDED){
+
+        }
+    }
+
+    public void onResume(){
+        super.onResume();
+        EditWorkout.LoadDB load = new EditWorkout.LoadDB();
+        load.execute();
     }
 
     // function called when the add button is called.
--- app/src/main/java/com/cs477/project2_areyes24/UpdateExercise.java
+++ app/src/main/java/com/cs477/project2_areyes24/UpdateExercise.java
@@ -2,13 +2,91 @@ package com.cs477.project2_areyes24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UpdateExercise extends AppCompatActivity {
-
+    private Button cancel;
+    private TextView name;
+    private EditText reps, sets, weight, notes;
+    private Button update;
+    DatabaseOpenHelper helper;
+    Cursor cursor;
+    private int id;
+    private String workout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_exercise);
+        // initiate the update button
+        update = findViewById(R.id.update_workout);
+        update.setOnClickListener(this::onUpdate);
+        // initiate the cancel button
+        cancel = findViewById(R.id.cancel_button);
+        cancel.setOnClickListener(this::onCancel);
+        // initiate the text view
+        name = findViewById(R.id.name);
+        // initiate the edit text's
+        reps = findViewById(R.id.reps);
+        sets = findViewById(R.id.sets);
+        weight = findViewById(R.id.weight);
+        notes = findViewById(R.id.notes);
+        helper = new DatabaseOpenHelper(this);
+        cursor = helper.readItems();
+        Bundle extras = getIntent().getExtras();
+        id = 0 ;
+        int index = 0;
+        if(extras != null){
+            id = extras.getInt("_id");
+            index = extras.getInt("index");
+        }
+        cursor.moveToPosition(index);
+        name.setText(cursor.getString(1));
+        workout = cursor.getString(1);
+        reps.setHint(Integer.toString(cursor.getInt(2)));
+        sets.setHint(Integer.toString(cursor.getInt(3)));
+        weight.setHint(Integer.toString(cursor.getInt(4)));
+        notes.setHint(cursor.getString(5));
+    }
+
+    // implement an update button
+    public void onUpdate(View view){
+        String repetitions = reps.getText().toString();
+        String s = sets.getText().toString();
+        String w = weight.getText().toString();
+        String n = notes.getText().toString();
+        int reps_ = -1, sets_ = -1 , weight_ = -1;
+        try {
+            if(!repetitions.isEmpty()){
+                reps_ = Integer.parseInt(repetitions);
+            }
+            if(!s.isEmpty()) {
+                sets_ = Integer.parseInt(s);
+            }
+            if(!w.isEmpty()) {
+                weight_ = Integer.parseInt(w);
+            }
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        Intent intent = new Intent();
+        intent.putExtra("reps", reps_);
+        intent.putExtra("sets", sets_);
+        intent.putExtra("weight", weight_);
+        intent.putExtra("notes", n);
+        intent.putExtra("name", workout);
+        intent.putExtra("_id", id);
+        setResult(EditWorkout.UPDATED, intent);
+        finish();
+    }
+
+    // implement a return to the previous activity without saving content.
+    public void onCancel(View view){
+        finish();
     }
 }
